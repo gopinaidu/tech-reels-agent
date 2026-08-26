f419a351f6a50e12f5a755f283e889747759cb56
gopinaidu <gopibhushan@gmail.com>
2026-08-26 16:54:29 -0400

    Export evidence-aware script writer

--- src/reelagent/scripting/__init__.py
+++ src/reelagent/scripting/__init__.py
@@ -2,9 +2,21 @@
 
 from reelagent.scripting.models import ScriptClaimDirective, ScriptClaimPlan
 from reelagent.scripting.planner import build_script_claim_plan
+from reelagent.scripting.writer import (
+    LlmScriptWriter,
+    ReelScriptDraft,
+    ScriptAttribution,
+    ScriptBeat,
+    ScriptWriterOutputError,
+)
 
 __all__ = [
+    "LlmScriptWriter",
+    "ReelScriptDraft",
+    "ScriptAttribution",
+    "ScriptBeat",
     "ScriptClaimDirective",
     "ScriptClaimPlan",
+    "ScriptWriterOutputError",
     "build_script_claim_plan",
 ]
